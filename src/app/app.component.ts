b01ef3fc3eadd9e9bb0ae77b8a4862f0f1816560
sfujimoto <unicoshun@gmail.com>
2016-10-16 22:43:26 +0900

    move resources to assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,21 +24,21 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        'resources/images/products/black-shoes.jpg',
+        'assets/resources/images/products/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         29.99
       ),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        'resources/images/products/blue-jacket.jpg',
+        'assets/resources/images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         29.99
       ),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        'resources/images/products/black-hat.jpg',
+        'assets/resources/images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99
       )
