835e8ee30094cd90d3305bda10db7a8f57a5c1fa
sfujimoto <unicoshun@gmail.com>
2016-10-17 11:05:15 +0900

    add ngmodel sku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <demo-from-sku></demo-from-sku>
+`
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DemoFromSkuComponent } from './demo-from-sku.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DemoFromSkuComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
