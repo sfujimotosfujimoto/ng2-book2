48a0d1199b34b662907c2e616adb18c662a9b074
sfujimoto <unicoshun@gmail.com>
2016-10-17 10:53:22 +0900

    start forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,10 @@
 import { Component } from '@angular/core';
-import {Product} from "./product.model";
-
-
-
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="inventory-app">
-      <products-list
-        [productList]="products"
-        (onProductSelected)="productWasSelected($event)"
-      >
-    </products-list>
-    </div>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  products: Product[];
-
-  constructor() {
-    this.products = [
-      new Product(
-        'MYSHOES',
-        'Black Running Shoes',
-        'assets/resources/images/products/black-shoes.jpg',
-        ['Men', 'Shoes', 'Running Shoes'],
-        29.99
-      ),
-      new Product(
-        'NEATOJACKET',
-        'Blue Jacket',
-        'assets/resources/images/products/blue-jacket.jpg',
-        ['Women', 'Apparel', 'Jackets & Vests'],
-        29.99
-      ),
-      new Product(
-        'NICEHAT',
-        'A Nice Black Hat',
-        'assets/resources/images/products/black-hat.jpg',
-        ['Men', 'Accessories', 'Hats'],
-        29.99
-      )
-    ];
-  }
-
-  productWasSelected(product: Product): void {
-    console.log('Product clicked: ', product);
-  }
+  title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list.component';
-import { ProductRowComponent } from './product-row.component';
-import { ProductImageComponent } from './product-image.component';
-import { ProductDepartmentComponent } from './product-department.component';
-import { PriceDisplayComponent } from './price-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent,
-    ProductRowComponent,
-    ProductImageComponent,
-    ProductDepartmentComponent,
-    PriceDisplayComponent
   ],
   imports: [
     BrowserModule,
