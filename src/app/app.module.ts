e7bd74e3816295a4d6b472f42efb67a0de2e24d4
sfujimoto <unicoshun@gmail.com>
2016-10-20 21:58:15 +0900

    start spotify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SimpleHttpComponent } from './simple-http.component';
-import {YoutubeService} from "./youtube.service";
-import { SearchBoxComponent } from './search-box.component';
-import { SearchResultComponent } from './search-result.component';
-import { YoutubeSearchComponent } from './youtube-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent,
-    SearchBoxComponent,
-    SearchResultComponent,
-    YoutubeSearchComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [YoutubeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
